81f8322dcd0bb5280a5959d0d9b9d584429edb7e
Sanskriti <sanskritisri27@gmail.com>
2024-12-04 17:38:41 +0530

    completed q13 for ES6 assignment

--- ES6/q13.js
+++ ES6/q13.js
@@ -1,1 +1,35 @@
-const people = [ { name: "Alice", address: { city: "New York", street: { name: "Broadway", number: 123 } } }, { name: "Bob", address: { city: "Los Angeles", street: { name: "Sunset Boulevard", number: 456 } } } ];
+const people = [
+    {
+        name: "Alice",
+        address: {
+            city: "New York",
+            street: {
+                name: "Broadway",
+                number: 123
+            }
+        }
+    },
+
+    {
+        name: "Bob",
+        address: {
+            city: "Los Angeles",
+            street: {
+                name: "Sunset Boulevard",
+                number: 456
+            }
+        }
+
+
+    }
+];
+
+let greetingsArr = [];
+
+// for the first person
+for(const {name, address:{city}, address:{street:{name:streetName}}} of people){
+    greetingsArr.push(`${name} lives in ${city} on ${streetName}`);
+}
+
+console.log(greetingsArr);
+
