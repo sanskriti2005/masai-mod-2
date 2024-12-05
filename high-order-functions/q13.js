b5f894ad129afd639cc40635a2e30626114e0cdb
Sanskriti <sanskritisri27@gmail.com>
2024-12-05 13:08:21 +0530

    solved extra challenge for q13

--- high-order-functions/q13.js
+++ high-order-functions/q13.js
@@ -3,4 +3,9 @@ let countOfEachProduct = products.reduce((acc, ele, i) => {
     acc[ele] ? acc[ele] += 1: acc[ele] = 1;
     return acc;
 },{})
-console.log(countOfEachProduct);
+// console.log(countOfEachProduct);
+
+// Extra challenge
+let productCount = Object.entries(countOfEachProduct);
+let sortedProductCount = productCount.sort((a,b) => b[1]-a[1])
+console.log(sortedProductCount);
