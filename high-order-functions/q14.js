4f85a609c570fdd6423045e12360a5379a9408cc
Sanskriti <sanskritisri27@gmail.com>
2024-12-06 09:17:23 +0530

    q14 file

---
+++ high-order-functions/q14.js
@@ -0,0 +1,8 @@
+const employeeDeatils = [
+    { name: "Alice", tasksCompleted: 8, rating: 4.7 },
+    { name: "Bob", tasksCompleted: 4, rating: 4.0 },
+    { name: "Charlie", tasksCompleted: 6, rating: 3.5 },
+    { name: "David", tasksCompleted: 10, rating: 4.9 },
+    { name: "Eve", tasksCompleted: 7, rating: 2.8 }
+
+]
